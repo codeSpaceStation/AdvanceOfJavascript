2ce046e8f15f86a20c084725a9ddc4e17db924b2
Muhammad Faturahman Bancin <mufatraba@gmail.com>
2024-02-20 14:31:12 +0700

    Add & Learn the Base of Closure

--- closures/script.js
+++ closures/script.js
@@ -3,7 +3,7 @@
 // console.log(nama);
 // var nama = 'andi';
 
-// Creation phase pada Global Context
+/** Creation phase pada Global Context */
 
 // name of var = undefined
 // nama function = fn()
@@ -19,15 +19,99 @@
 // Execution phase
 
 // The others ex:
-console.log(sayHello());
 
-var nama = 'Dika';
-var umur = 16;
+// console.log(sayHello());
+// var nama = 'Dika';
+// var umur = 16;
 
-function sayHello(){
-    console.log(`Halo, nama saya ${nama}, saya ${umur} tahun.`);
-}
+
+// function sayHello(){
+//     return `Halo, nama saya ${nama}, saya ${umur} tahun.`;
+// }
 
 // var sayHello = () =>{
 //     return `Hello nama saya ${nama}, saya ${umur} tahun.`;
 // }
+
+/**
+ * Ketika di visualizer ketika saat masuk menjalankan function,
+ * Func seolah" menjalankan execution context sendiri
+ * Jd jika ketemu func executenya gk global lagi
+ */
+ // Maka jika ketemu func, dia seolah":
+// Func membuat Local Execution context
+// Yang didalamnya terdapat juga creation dan execution phase
+// Bedanya dengan local execution context:
+// selain kita bisa akses WINDOW
+// kita juga bisa akses ARGUMENTS
+// didalamnya juga terdapat hoisting (local)
+
+
+/*### Another Ex / study case: */
+// var nama = 'dika';
+// var username = '@dikakeren';
+
+// function cetakUrl(username){
+//     var instagramUrl = 'https://instagram.com/';
+//     return instagramUrl + username;
+// }
+
+// console.log(cetakUrl(username));
+
+
+
+/*### Another Ex / study case: */
+// function a(){
+//     console.log('ini func A');
+
+//     function b(){
+//         console.log('ini func B');
+
+//         function c(){
+//             console.log('ini func C');
+//         }
+
+//         c();
+//     }
+
+//     b();
+// }
+
+// a();
+
+
+/* ### Another Ex: */
+// var nama = 'dika';
+// var username = '@dikakeren';
+
+// function cetakUrl(){
+//   // console.log(arguments);
+//   var instagramUrl = 'https://instagram.com/';
+//   return instagramUrl + username;
+// }
+
+// console.log(cetakUrl('@dededoremi', '@budi'));
+
+/* exercise: */
+function callName(){
+  var nama = 'dika';
+  console.log(nama);
+}
+
+function callNameTwo(){
+  console.log(nama);
+}
+
+
+console.log(nama);
+var nama = 'dede';
+callName();
+callNameTwo('karina');
+console.log(nama);
+
+
+// Output :
+// undefined
+// dika
+// dede
+// dede
