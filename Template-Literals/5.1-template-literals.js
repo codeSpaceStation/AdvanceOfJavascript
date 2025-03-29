edb24de34497f5d7e1df158d0b4c24cde7b852ff
Muhammad Faturahman Bancin <bancin2001@gmail.com>
2025-03-29 17:58:31 +0700

    Learn Template Literals

---
+++ Template-Literals/5.1-template-literals.js
@@ -0,0 +1,27 @@
+//// Template Literals / Template String
+// const nama = "Sela";
+// const umur = 23;
+// console.log(`Halo, Nama saya ${nama}, Umur saya ${umur} tahun.`);
+// /// Sebelum ada Template literals
+// console.log("Halo, Nama saya " + nama + " Umur saya " + umur + " tahun.");
+
+/// Embeded Expressions
+// console.log(`${1 + 3}`);
+// console.log(`${alert("halo!")}`);
+// const x = 11;
+// console.log(`${x % 2 == 0 ? "Genap" : "Ganjil"}`);
+
+/// HTML Fragments
+// const mhs = {
+// 	nama: "Sela",
+// 	umur: 23,
+// 	nim: "1920406023",
+// 	email: "sela@gmail.com",
+// };
+
+// const el = `<div class="mhs">
+// <h2>${mhs.nama}</h2>
+// <span class="nim">${mhs.nim}</span>
+// </div>`;
+
+// console.log(el);
